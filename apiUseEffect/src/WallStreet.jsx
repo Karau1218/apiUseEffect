0f1dccdd536f8dd4459fb33160943ac639cdd23d
Fkarau <30944677+Karau1218@users.noreply.github.com>
2026-05-12 11:31:19 -0700

    added code in header, and wallstreet jsx

---
+++ apiUseEffect/src/WallStreet.jsx
@@ -0,0 +1,14 @@
+import { useState, useEffect } from "react";
+
+export default function WallStreet() {
+  const [author, title] = useState({
+
+  })
+  const []
+
+  useEffect(() => {
+    fetch("
+https://newsapi.org/v2/everything?domains=wsj.com&apiKey=4dda406511e34600832f0c3316ead937")
+  })
+}
+
